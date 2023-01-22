d6e2c3019453172e6a4ca1f78f3fa355d8cf7c40
Niklas Gaertner <niklas@gaertner.de.com>
2023-01-22 22:19:34 +0000

    database test

---
+++ test_db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+engine = create_engine("mysql:///root@localhost/first_db")
+
+df = pd.DataFrame(
+    {
+        'a': [1,2,3], 
+        'b': [4,5,6]
+    }
+
+
+)
+
+with engine.begin as con:
+    df.to_sql('test_table', con)
