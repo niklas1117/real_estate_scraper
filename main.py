827f6fd611b0efd6e71792330b7e7b5c06465540
Niklas Gaertner <niklas@gaertner.de.com>
2023-01-29 15:52:12 +0000

    added rent to main

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ regions = get_regions()
 rms = RightmoveScraper(buy=True)
 rms.scrape_regions(regions, save=True, verbose=True)
 
-# rms = RightmoveScraper(False)
-# df = rms.scrape_regions(regions, save=True, verbose=True)
-# print(df)
+rms = RightmoveScraper(False)
+rms.scrape_regions(regions, save=True, verbose=True)
